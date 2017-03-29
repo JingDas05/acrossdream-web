70e865a10117861ad1bb436587ba0c51e8c257b0
wusi06 <wusi0109@163.com>
2017-03-29 13:03:49 +0800

    添加elementUi

--- src/views/HelloComponent.vue
+++ src/views/HelloComponent.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     helloComponent
+    <el-button type="text" @click="open">点击打开 Message Box</el-button>
   </div>
 </template>
 <script>
@@ -8,8 +9,20 @@ export default {
   data () {
     return {}
   },
+  methods: {
+    open () {
+      this.$alert('这是一段内容', '标题名称', {
+        confirmButtonText: '确定',
+        callback: action => {
+          this.$message({
+            type: 'info',
+            message: `action:${action}`
+          })
+        }
+      })
+    }
+  },
   components: {
-
   }
 }
 </script>
