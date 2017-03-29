6eee0395cee3554e29d6435a420814d3287b1139
wusi06 <wusi0109@163.com>
2017-03-29 13:11:49 +0800

    添加resource　和 vuex

--- src/main.js
+++ src/main.js
@@ -5,14 +5,18 @@ import App from './App'
 import router from './router'
 import elementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
+import VueResource from 'vue-resource'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 Vue.use(elementUI)
+Vue.use(VueResource)
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: {App}
 })
