6b7d99e261aba0704df8612f7d791afdfba9acbf
wusi05 <wusi05@126.com>
2017-04-15 22:49:02 +0800

    添加创建日记

--- src/views/DiaryCreate.vue
+++ src/views/DiaryCreate.vue
@@ -1,16 +1,72 @@
 <template>
-  <div>
-    创建日记
+  <div style="float: left; width: 40%">
+    <el-form label-width="100px" :model="diary" :rules="rules" ref="diary" label-position="left">
+      <el-form-item label="日记名称" prop="name">
+        <el-input v-model="diary.name"></el-input>
+      </el-form-item>
+      <el-form-item label="描述" prop="description">
+        <el-input v-model="diary.description"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm()">创建</el-button>
+        <el-button @click="resetForm()">重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 <script>
 export default {
   data () {
-    return {}
+    return {
+      diary: {
+        authorId: '',
+        name: '',
+        description: ''
+      },
+      rules: {
+        name: [
+          {required: true, message: '请输入日记名称', trigger: 'blur'},
+          {min: 1, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur'}
+        ],
+        description: [
+          {required: true, message: '请输入日记描述', trigger: 'change'},
+          {min: 1, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur'}
+        ]
+      }
+    }
   },
   components: {},
   computed: {},
-  methods: {},
+  methods: {
+    resetForm () {
+      this.diary = {}
+    },
+    submitForm () {
+      this.requestCreateDiary(this.diary.name, this.diary.description)
+    },
+    requestCreateDiary (name, description) {
+      this.$http.post('/tg/api/diaries/create',
+        {
+          authorId: '12345678123456781234567812345678',
+          name: name,
+          description: description
+        }
+      ).then(response => {
+        if (response.body.id !== 'undefined') {
+          this.$notify({
+            title: '成功',
+            message: '创建日记成功',
+            type: 'success'
+          })
+        }
+      }, response => {
+        this.$notify.error({
+          title: '错误',
+          message: '创建日记失败，请联系管理员，邮箱wusi0109@163.com'
+        })
+      })
+    }
+  },
   created () {
   },
   activated () {
--- src/views/PageCreate.vue
+++ src/views/PageCreate.vue
@@ -1,19 +1,69 @@
 <template>
   <div>
-    创建记录
+    <el-select v-model="pageParams.diaryId" clearable placeholder="请选择日记">
+      <el-option
+        v-for="(diary, index) in diaries"
+        :key="diary.id"
+        :label="diary.name"
+        :value="diary.id">
+      </el-option>
+    </el-select>
   </div>
 </template>
 <script>
 export default {
   data () {
-    return {}
+    return {
+      diaries: [],
+      pageParams: {
+
+      }
+    }
   },
   components: {},
   computed: {},
-  methods: {},
+  methods: {
+    requestDiaries (userId, pageNum, pageSize) {
+      this.$http.post('/tg/api/diaries',
+        {
+          userId: userId,
+          pageNum: pageNum,
+          pageSize: pageSize
+        }
+      ).then(response => {
+        this.diaries = response.body.data
+      }, response => {
+        console.error(response)
+      })
+    }
+//    requestCreatePage () {
+//      this.$http.post('/tg/api/diaries/create',
+//        {
+//          authorId: '12345678123456781234567812345678',
+//          name: name,
+//          description: description
+//        }
+//      ).then(response => {
+//        if (response.body.id !== 'undefined') {
+//          this.$notify({
+//            title: '成功',
+//            message: '创建日记成功',
+//            type: 'success'
+//          })
+//        }
+//      }, response => {
+//        this.$notify.error({
+//          title: '错误',
+//          message: '创建日记失败，请联系管理员，邮箱wusi0109@163.com'
+//        })
+//      })
+//    }
+  },
   created () {
+    this.requestDiaries('12345678123456781234567812345678', this.$consts.pageNum, this.$consts.pageSize)
   },
   activated () {
+    console.log('activated')
   },
   deactivated () {
   }
